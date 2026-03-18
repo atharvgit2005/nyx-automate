c7bb068d183fec1dc1428cbab9259e034e7ccf0f
Atharv Paharia <pahariaatharv2005@gmail.com>
2026-03-18 10:47:15 +0530

    Fix next/image: add OAuth image hostnames to remotePatterns
    - lh3.googleusercontent.com (Google)
    - avatars.githubusercontent.com (GitHub)
    - pbs.twimg.com (Twitter/X)

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,21 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'avatar.vercel.sh',
       },
+      // Google OAuth avatars
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      // GitHub OAuth avatars
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      // Twitter/X OAuth avatars
+      {
+        protocol: 'https',
+        hostname: 'pbs.twimg.com',
+      },
     ],
   },
 };
