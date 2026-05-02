1a2234f92ee6550247c8ca45cfbceb67d6ed7525
Atharv Paharia <pahariaatharv2005@gmail.com>
2026-05-03 04:33:44 +0530

    fix(seo): add non-www to www redirect

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'nyxstudio.tech',
+          },
+        ],
+        destination: 'https://www.nyxstudio.tech/:path*',
+        permanent: true,
+      },
+    ];
+  },
   /* config options here */
   outputFileTracingRoot: __dirname,
   typescript: {
