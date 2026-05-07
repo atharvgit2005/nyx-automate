f624d6e718df7cc4fdb104f3265ba0ad2957d6d0
Atharv Paharia <pahariaatharv2005@gmail.com>
2026-05-07 17:03:12 +0530

    fix(ts): import JWT type from next-auth/jwt

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,5 @@
 import { AuthOptions } from "next-auth"
+import { JWT } from "next-auth/jwt"
 import GoogleProvider from "next-auth/providers/google"
 import CredentialsProvider from "next-auth/providers/credentials"
 import bcrypt from "bcryptjs"
